1db907e4a1f5136222cc3a441cd05d8f2dfa25b1
Jayasree <jayasree.job@gmail.com>
2017-11-09 10:53:42 -0600

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './about/education/education.component';
+import { SkillsComponent } from './about/skills/skills.component';
+import { CertificationsComponent } from './about/certifications/certifications.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    EducationComponent,
+    SkillsComponent,
+    CertificationsComponent,
+    ExperienceComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'experience', component: ExperienceComponent },
+      { path: 'contact', component: ContactComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
